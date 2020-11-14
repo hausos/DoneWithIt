43429ef9f75c26c70504448a3c761ece3f7921d3
Hausos <aurora.makovac@gmail.com>
2020-11-14 17:37:24 +0100

    Adding background

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, Platform, StatusBar, Dimensions} from 'react-native';
+import {View} from 'react-native';
 import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  const { height, width } = useDimensions().window;
-  const landscape = width > height;
-
-  return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: "100%",
-        height: landscape ? "100%" : "30%",
-      }}>
-
-      </View>
-
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
 
-const containerStyle = {backgroundColor: 'orange'};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-
-  },
-});
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require("../assets/background.jpg")}
+        >
+            <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+    logo: {
+        width: 100,
+        height: 100,
+    },
+    loginButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "#fc5c65",
+    },
+    registerButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "#4ecdc4",
+    },
+})
+
+
+export default WelcomeScreen; 
