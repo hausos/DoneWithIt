7b1ac95b3f40d3c5c7c321c72e071216e6df5cad
Hausos <aurora.makovac@gmail.com>
2020-11-22 03:04:26 +0100

    adding messages list

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Platform, StatusBar } from 'react-native';
 
 import ListItem from '../components/ListItem';
 
@@ -20,16 +20,27 @@ const messages = [
 
 function MessagesScreen(props) {
     return (
-        <FlatList 
-            data={messages} keyExtractor={message => message.id.toString()}
-            renderItem={({item}) => <ListItem
-            title={item.title}
-            subTitle={item.description}
-            image={item.image}
-            
-            />}
-        />
+        <SafeAreaView style={styles.screen}>
+
+            <FlatList 
+                data={messages} keyExtractor={message => message.id.toString()}
+                renderItem={({item}) => <ListItem
+                title={item.title}
+                subTitle={item.description}
+                image={item.image}
+                
+                />}
+            />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+
+    }
+    
+})
 
 export default MessagesScreen;
